ff9a47e86d0f55b374805f4e1b0d1c78717cf9b4
swen <1063642821@qq.com>
2020-04-07 15:52:17 +0800

    初始化

--- src/main/java/com/tnb/security/demo/config/SecurityConfig.java
+++ src/main/java/com/tnb/security/demo/config/SecurityConfig.java
@@ -6,7 +6,9 @@ import com.tnb.security.demo.handler.MyAuthenticationSucessHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,6 +20,8 @@ import javax.annotation.Resource;
  * @author swen
  */
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${web.anon-url}")
